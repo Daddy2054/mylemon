d670b38406cc8d5f355a3fd5a02beef4f517d8ff
Jean <acalto2@gmail.com>
2024-04-14 13:55:28 -0400

    adding API

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -17,8 +17,17 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework import routers
+from restaurant import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("restaurant/", include("restaurant.urls")),
+]
+router = DefaultRouter()
+router.register(r"tables", views.BookingViewSet)
+
+urlpatterns += [
+    path("restaurant/booking/", include(router.urls)),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateAPIView,
+    DestroyAPIView,
+)
+from .models import Menu, Booking
+
+from .serializers import (
+    MenuSerializer,
+    BookingSerializer,
+    UserSerializer,
+)
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
+    return render(request, "index.html", {})
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
